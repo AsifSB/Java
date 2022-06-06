da6bd27f10bc3daa672130db25c44f9c898bfb03
Kiran Thamballa <kiran.thamballa@gmail.com>
2022-06-06 13:58:09 +0530

    Updated parent class constructor

--- SGHGROUP/src/com/sghJed/Parent.java
+++ SGHGROUP/src/com/sghJed/Parent.java
@@ -2,6 +2,10 @@ package com.sghJed;
 
 public class Parent {
 	
+	public Parent(){
+		System.out.println("This is a parent class constructor");
+	}
+	
 	public static int displayMyLuckyNumber() {
 		return 1111111;
 	}
